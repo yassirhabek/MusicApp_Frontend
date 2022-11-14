9d32e45dfb089a894e05199eb766478289899eed
Yassir Habek <485050@student.fontys.nl>
2022-11-14 19:49:24 +0100

    login route made

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,45 +6,15 @@ import classes from "../css/Login.module.css";
 
 const DashboardLoginLightTheme = () => {
   return (
-    <div className={classes.dashboardLoginLightTheme}>
-      <img className={classes.vectorsIcon} alt="" src="../vectors.svg" />
-      <img
-        className={classes.lightModeDarkMode}
-        alt=""
-        src="../light-mode--dark-mode.svg"
-      />
-      <form className={classes.form} action="Login" method="post">
-        <div className={classes.sectionForgotDiv}>
-          <div className={classes.forgotPasswordDiv}>Forgot password?</div>
-          <div className={classes.rememberMeDiv}>Remember me</div>
-          <div className={classes.rectangleDiv} />
-        </div>
-        <button className={classes.loginBtnButton}>
-          <Button
-            className={classes.rectangleButton}
-            sx={{ width: 300 }}
-            variant="contained"
-            color="success"
-            size="large">
-            Login
-          </Button>
-          <div className={classes.loginDiv}>login</div>
-        </button>
-        <input
-          className={classes.passwordInput}
-          type="password"
-          placeholder="Password"
-        />
-        <input
-          className={classes.usernameInput}
-          type="text"
-          placeholder="Login"
-        />
-        <div className={classes.signInAndStartManagingYou}>
-          Sign in and start managing your candidates!
-        </div>
-        <div className={classes.signInDiv}>Sign in</div>
-      </form>
+    <div className={classes.container}>
+        <h2>Login</h2>
+        <h3>Enter your credentials</h3>
+        <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <a href="#">Forgot Password?</a>
+            <Button variant="contained" color="primary">Login</Button>
+        </form>
     </div>
   );
 };
